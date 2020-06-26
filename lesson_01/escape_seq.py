563597a483860c78e6786d2d4f6c155995ec9b7e
kbakhchedzhy <67337899+kbakhchedzhy@users.noreply.github.com>
2020-06-26 20:56:35 +0300

    Delete escape_seq.py

--- lesson_01/escape_seq.py
+++
@@ -1,8 +0,0 @@
-print('Escape seguence\n\
-\\a\tBell (alert)\n\
-\\b\tBackspace\n\
-\\n\tNew line\n\
-\\t\tHorizontal tab\n\
-\\\\ \tBackslash \\ \n\
-\\\"\tDouble quotation mark \"\n\
-\\\'\t Single quotation mark\'')
