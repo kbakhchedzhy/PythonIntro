dd01b20291cde65036e4afcae907389e6078f680
kbakhchedzhy <k.bakhchedzhy@gmail.com>
2020-07-26 00:28:41 +0300

    26.07

---
+++ lesson_07/hw_12.py
@@ -0,0 +1,13 @@
+from random import randint
+
+lst = [randint(1, 100) for _ in range(10)]
+print('Список: \n', lst)
+k = int(input('Введите индекс элемента для удаления: '))
+
+for el in range(len(lst)-k-1):
+    lst[k] = lst[k+1]
+    k += 1
+lst.pop()
+# lst = lst[:k] + lst[k+1:10]
+# lst.remove(lst[k])
+print(lst)
