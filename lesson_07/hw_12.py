a9a82163b65e1f56705bd89a2486e27d3d18def9
kbakhchedzhy <k.bakhchedzhy@gmail.com>
2020-07-26 20:48:05 +0300

    26.07

--- lesson_07/hw_12.py
+++ lesson_07/hw_12.py
@@ -5,9 +5,8 @@ lst = [randint(1, 100) for _ in range(length)]
 print('Список: \n', lst)
 k = int(input('Введите индекс элемента для удаления: '))
 
-for el in range(length-k-1):
-    lst[k] = lst[k+1]
-    k += 1
+for el in range(k, length-1):
+    lst[el] = lst[el+1]
 lst.pop()
 # lst = lst[:k] + lst[k+1:10]
 # lst.remove(lst[k])
