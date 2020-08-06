9aeee6f8f4967784add4f23592d04a97b75b9d2f
kbakhchedzhy <k.bakhchedzhy@gmail.com>
2020-08-06 11:56:40 +0300

    06.08

---
+++ lesson_09/hw_21.py
@@ -0,0 +1,10 @@
+from math import sqrt
+def square(a):
+    P = a * 4
+    S = a * a
+    d = a * sqrt(2)
+    t = (P, S, d)
+    return t
+
+a = int(input('Введите сторону квадрата: '))
+print(square(a))
