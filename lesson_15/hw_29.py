7d541755f1f186434f8df65ce2c9eb6c38eb3c2c
kbakhchedzhy <k.bakhchedzhy@gmail.com>
2020-08-31 10:19:56 +0300

    31.08

--- lesson_15/hw_29.py
+++ lesson_15/hw_29.py
@@ -1,19 +1,20 @@
 
 class Counter:
-    current = 0
 
     def __init__(self, min_v, max_v):
         self.min_v = min_v
         self.max_v = max_v
+        self.current = min_v
 
     def count(self):
-        if not self.current:
-            self.current = self.min_v
+        if self.current == self.min_v:
+            print(self.current)
+
         if self.current <= self.max_v - 1:
             self.current += 1
             return self.current
 
 
-cnt = Counter(0, 100)
+cnt = Counter(-2, 100)
 for _ in range(cnt.min_v, cnt.max_v):
     print(cnt.count())
